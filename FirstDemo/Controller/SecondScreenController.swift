2be2c0546d8f8b38e3434dd7ce0f52ef63713cdd
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-12 13:10:56 -0600

    Made a new screen and made a new class to work with that screen.

---
+++ FirstDemo/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  FirstDemo
+//
+//  Created by Pearson, William on 10/12/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class SecondScreenController : UIViewController
+{
+    
+}
