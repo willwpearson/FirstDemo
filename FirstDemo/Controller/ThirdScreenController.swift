61b4ff06133842aa65672d7c66def7f87c5518bc
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-18 11:57:01 -0600

    Added a new screen and added a few elements to that screen. Created a new segue to and from that screen, and also linked it to a new code file.

---
+++ FirstDemo/Controller/ThirdScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  ThirdScreenController.swift
+//  FirstDemo
+//
+//  Created by Pearson, William on 10/18/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ThirdScreenController : UIViewController
+{
+    
+}
